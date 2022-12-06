88fb875de3af7501f7d51a5c969b1b85ec90a7bb
Khaprel <59897819+Khaprel@users.noreply.github.com>
2022-12-06 05:18:47 -0800

    Update solution.py

--- Day06/solution.py
+++ Day06/solution.py
@@ -1,1 +1,13 @@
+with open('./Day06/input.txt', 'r') as file:
+    data = file.readline()
 
+def tuner(signal, marker):
+    '''Receives a str, num. Returns end num of span without duplicate characters in string'''
+    for i, char in enumerate(signal):
+        packet = []
+        packet.extend(signal[i:i+marker])
+        if len(packet) == len(set(packet)):
+            return i+marker
+
+print(f'The start of packet marker was detected at character: {tuner(data,4)}')
+print(f'The start of message marker was detected at character: {tuner(data,14)}')
