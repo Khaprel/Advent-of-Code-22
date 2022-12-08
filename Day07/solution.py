2ab378265cc3d58d73bab7f632892a887c3c4de2
Khaprel <59897819+Khaprel@users.noreply.github.com>
2022-12-07 20:52:22 -0800

    Refactored to functions

--- Day07/solution.py
+++ Day07/solution.py
@@ -1,74 +1,87 @@
-with open('./Day07/input.txt', 'r') as file:
-    data = file.readlines()
+'''Opens and parses data from terminal,
+creating a file heiarchy to determine which folder to delete'''
+with open('./Day07/input.txt', 'r', encoding='utf-8') as file:
+    dir_data = file.readlines()
 
-filesystem = {}
-cur_dir = [''] #List of directory path
+def parse_directory(data):
+    '''Parses data into dictionary and totals size of files'''
+    system = {}
+    cur_dir = [''] #List of directory path
+    for line in data:
+        if '$ ls' in line:
+            continue
+        elif '$ cd ..' in line: #Removes the last directory in the path
+            cur_dir.pop()
+            # print(cur_dir)
+        elif '$ cd /' in line: #Clears the directory and returns to root
+            cur_dir.clear()
+            cur_dir.append('root')
+        elif '$ cd' in line: #Adds the current directory to the new path and appends to directory
+            cur_dir.append(str(cur_dir[-1]) + '/' + str(line.strip().split(' ')[2]))
+            # print(line.strip().split(' ')[2])
+        elif 'dir ' in line: #Creates a directory with a value of 0
+            path = tuple(cur_dir[-1].split('/'))
+            system.update({path: system.get(path, 0)})
+        else: #Creates/adds size of contents to directory
+            # print(line.strip(), tuple(cur_dir[-1].split('/')))
+            size = line.strip().split(' ')[0]
+            path = tuple(cur_dir[-1].split('/'))
+            try:
+                size = int(size)
+                system.update({path: system.get(path, 0)+size})
+            except TypeError as err:
+                print("Unexpected input: ", err)
+    return system
 
-for line in data:
-    if '$ ls' in line:
-        continue
-    elif '$ cd ..' in line: #Removes the last directory in the path
-        cur_dir.pop()
-        # print(cur_dir)
-    elif '$ cd /' in line: #Clears the directory and returns to root
-        cur_dir.clear()
-        cur_dir.append('root')
-    elif '$ cd' in line: #Adds the current directory string to the new path and appends to directory list
-        cur_dir.append(str(cur_dir[-1]) + '/' + str(line.strip().split(' ')[2]))
-        # print(line.strip().split(' ')[2])
-    elif 'dir ' in line: #Creates a directory with a value of 0
-        path = tuple(cur_dir[-1].split('/'))
-        filesystem.update({path: filesystem.get(path, 0)})
-    else: #Creates/adds size of contents to directory
-        # print(line.strip(), tuple(cur_dir[-1].split('/')))
-        size = line.strip().split(' ')[0]
-        path = tuple(cur_dir[-1].split('/'))
-        try:
-            size = int(size)
-            filesystem.update({path: filesystem.get(path, 0)+size})
-        except TypeError as err:
-            print("Unexpected input: ", err)
+def add_dir_val(dic):
+    '''Sums up the total values for each directory'''
+    totals = {}
+    for k, val in dic.items():
+        totals[k] = totals.get(k, 0)+val
+    return totals
 
-file_totals = {}
-# print(filesystem)
-for k, v in filesystem.items():
-    #Adds the total values for each directory
-    file_totals[k] = file_totals.get(k, 0)+v
+def add_child_val_to_parent(dic):
+    '''Modifies dic in place to add the size of the child directory to the parent directory'''
+    for k, val in dic.items():
+        if len(k) > 1:
+            cap = k[0:(len(k)-1)]
+            dic.update({cap:dic.get(cap)+val})
+            while len(cap) > 1:
+                cap = cap[0:(len(cap)-1)]
+                dic.update({cap:dic.get(cap)+val})
 
-for k, v in file_totals.items():
-    # print(f'{k} | {v}')
-    if len(k) > 1:
-        cap = k[0:(len(k)-1)]
-        # print(f'K = {k}, V = {v}')
-        # print(f'Cap: {cap} File: {file_totals.get(cap)} V:{v}')
-        try:
-            file_totals.update({cap:file_totals.get(cap)+v})
-        except TypeError as err:
-            print("Error: ", err)
-        while len(cap) > 1:
-            cap = cap[0:(len(cap)-1)]
-            file_totals.update({cap:file_totals.get(cap)+v})
+def sum_xsize_dir(num, dic):
+    '''Returns the sum of directories <= num'''
+    acc = 0
+    for data in dic.values():
+        if int(data) <= num:
+            acc += data
+    return acc
 
-acc = 0
-for data in file_totals.values():
-    # Adds the total of files that are <= 100000
-    if int(data) <= 100000:
-        acc += data
-print(f'The sum of directories of at most 100000: {acc}')
+
+filesystem = parse_directory(dir_data)
+
+file_totals = add_dir_val(filesystem)
+
+add_child_val_to_parent(file_totals)
+
+print(f'The sum of directories of at most 100000: {sum_xsize_dir(100000, file_totals)}')
 
 
 #Part 2
+def min_req_delete():
+    '''Creates a list of directories large enough, and returns the minium'''
+    delete_list = []
+    for data in file_totals.values():
+        # print(unused_space, update_space, update_space-unused_space)
+        if int(data) > needed_space:
+            delete_list.append(data)
+    return min(delete_list)
+
 used_space = file_totals[('root',)]
+TOTALSPACE = 70000000
+UPDATE_SPACE = 30000000
+unused_space = TOTALSPACE - used_space
+needed_space = UPDATE_SPACE - unused_space
 
-totalspace = 70000000
-update_space = 30000000
-unused_space = totalspace - used_space
-needed_space = update_space - unused_space
-
-delete_list = []
-for data in file_totals.values():
-    # print(unused_space, update_space, update_space-unused_space)
-    if int(data) > needed_space:
-        delete_list.append(data)
-
-print(f'The smallest directory that would free enough space is: {min(delete_list)}')
+print(f'The smallest directory that would free enough space is: {min_req_delete()}')
